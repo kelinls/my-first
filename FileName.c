9431e89aaa5cf27f6e623d8cf2e09a6de8ca3445
kelinls <140181630+kelinls@users.noreply.github.com>
2023-08-01 20:11:52 +0800

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+int main()
+{
+	int nmb1 = 0;
+	int nmb2 = 0;
+	int sum = 0;
+	scanf("%d %d", &nmb1, &nmb2);
+	sum = nmb1 + nmb2;
+	printf("x=%d", sum);
+
+
+
+
+	return 0;
+}
